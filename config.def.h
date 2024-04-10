3bfb678de835d1ebdee9cac25f96d00dc199c373
Technicfan <lenni.ahrens@web.de>
2024-04-10 18:39:50 +0200

    cleanup

--- config.def.h
+++ config.def.h
@@ -4,19 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrains:bold:pixelsize=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#d3869b", "#282828" },
+	[SchemeSel] = { "#282828", "#dfbf8e" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 28;
 static unsigned int min_lineheight = 8;
 
 /*
